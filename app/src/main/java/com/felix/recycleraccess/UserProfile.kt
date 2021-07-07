14d50ee870dc9b41ed465f64267232bd817baaee
fsonic202 <f.oseh@aol.com>
2021-07-07 16:11:32 +0100

    Assisgment Week5

---
+++ app/src/main/java/com/felix/recycleraccess/UserProfile.kt
@@ -0,0 +1,4 @@
+package dtos
+
+data class UserProfile(var surName:String, var otherName:String, var img:Int)
+
---
+++ app/src/test/java/com/felix/recycleraccess/UserProfile.kt
@@ -0,0 +1,3 @@
+package com.felix.recycleraccess
+
+class  UserProfile(var surName:String, var otherName:String, var img:Int)
